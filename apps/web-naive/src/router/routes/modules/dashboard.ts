0123840a252e0278db675f8c0ee45cce5010da7c
hzj <hezhijian249@qq.com>
2024-12-19 21:20:12 +0800

    feat: 添加路由配置

--- apps/web-naive/src/router/routes/modules/dashboard.ts
+++ apps/web-naive/src/router/routes/modules/dashboard.ts
@@ -38,6 +38,23 @@ const routes: RouteRecordRaw[] = [
         meta: {
           icon: 'carbon:workspace',
           title: $t('page.charts.title'),
+          keepAlive: true,
+        },
+      },
+      {
+        name: 'ChartsPreview',
+        path: '/charts/preview',
+        component: () => import('#/views/dashboard/preview/preview.vue'),
+        meta: {
+          icon: 'carbon:workspace',
+          title: $t('page.charts.title'),
+          keepAlive: true,
+          hideInMenu: true,
+        },
+        props: (route) => {
+          return {
+            cid: route.query.cid,
+          };
         },
       },
     ],
