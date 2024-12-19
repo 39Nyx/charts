99a24768c7151bb1fbe22870df5f5a1becc2081a
hzj <hezhijian249@qq.com>
2024-12-19 23:31:38 +0800

    feat: 添加图表类型过滤

--- apps/web-naive/src/views/dashboard/charts/charts.vue
+++ apps/web-naive/src/views/dashboard/charts/charts.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, type Ref, ref } from 'vue';
+import { computed, onMounted, type Ref, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 import { NInfiniteScroll } from 'naive-ui';
@@ -18,10 +18,21 @@ function query() {
     return;
   }
   currentPage.value++;
+  if (currentPage.value > 358) {
+    return;
+  }
   loading.value = true;
   getChartData(currentPage.value)
     .then((res: any) => {
-      chartList.value.push(...res.data);
+      const result = res.data.map((item: any) => {
+        return {
+          ...item,
+          builtinTags: JSON.parse(item.builtinTags).map((tag: string) =>
+            tag.replace('series-', ''),
+          ),
+        };
+      });
+      chartList.value.push(...result);
     })
     .finally(() => {
       loading.value = false;
@@ -46,18 +57,106 @@ function selectCharts(chart: any) {
     },
   });
 }
+
+const seriesTypes = [
+  {
+    name: '全部',
+    value: '',
+  },
+  {
+    name: '折线图',
+    value: 'line',
+  },
+  {
+    name: '柱状图',
+    value: 'bar',
+  },
+  {
+    name: '饼图',
+    value: 'pie',
+  },
+  {
+    name: '雷达图',
+    value: 'radar',
+  },
+  {
+    name: '散点图',
+    value: 'scatter',
+  },
+  {
+    name: '地图',
+    value: 'map',
+  },
+  {
+    name: '水球图',
+    value: 'liquidFill',
+  },
+];
+
+const selectedSeriesType: Ref<string[]> = ref(['']);
+
+function seriesTypeClick(item: { name: string; value: string }) {
+  const isSelected = !selectedSeriesType.value.includes(item.value);
+  if (isSelected) {
+    if (item.value === '') {
+      selectedSeriesType.value = [''];
+      return;
+    }
+    selectedSeriesType.value.push(item.value);
+    if (selectedSeriesType.value.includes('')) {
+      selectedSeriesType.value = selectedSeriesType.value.filter(
+        (v) => v !== '',
+      );
+    }
+  } else {
+    if (item.value === '') {
+      selectedSeriesType.value = [''];
+      return;
+    }
+    selectedSeriesType.value = selectedSeriesType.value.filter(
+      (v) => v !== item.value,
+    );
+    if (selectedSeriesType.value.length === 0) {
+      selectedSeriesType.value.push('');
+    }
+  }
+}
+
+const showChartList = computed(() => {
+  if (
+    selectedSeriesType.value.length === 1 &&
+    selectedSeriesType.value[0] === ''
+  ) {
+    return chartList.value;
+  }
+  return chartList.value.filter((item) =>
+    selectedSeriesType.value.some((v) => item.builtinTags.includes(v)),
+  );
+});
 </script>
 
 <template>
   <div ref="container" class="my-app-container">
+    <div class="chart-types">
+      <div>图表分类:</div>
+      <div
+        v-for="item in seriesTypes"
+        :key="item.value"
+        :class="{ active: selectedSeriesType.includes(item.value) }"
+        class="chart-type"
+        @click="seriesTypeClick(item)"
+      >
+        {{ item.name }}
+      </div>
+    </div>
     <NInfiniteScroll
       :distance="30"
-      :style="{ height: 'calc(100vh - 112px)' }"
+      :style="{ height: 'calc(100vh - 164px)' }"
       @load="query"
     >
       <div class="chart-container">
         <div
-          v-for="item in chartList"
+          v-for="item in showChartList"
           :key="item.cid"
           class="chart-item"
           @click="selectCharts(item)"
@@ -76,6 +175,29 @@ function selectCharts(chart: any) {
 <style scoped lang="scss">
 .my-app-container {
   padding: 12px;
+}
+
+.chart-types {
+  display: flex;
+  grid-gap: 8px;
+  padding: 12px 8px;
+  margin-bottom: 12px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0.0267rem 0.16rem 0 rgb(0 0 0 / 10%);
+
+  .chart-type {
+    margin-right: 8px;
+    cursor: pointer;
+
+    &.active {
+      color: #f20d0d;
+    }
+
+    &:hover {
+      color: #f20d0d;
+    }
+  }
 }
 
 .chart-container {
