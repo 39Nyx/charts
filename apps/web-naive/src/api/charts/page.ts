c60381aebeaa6301c75faafbbb714d8aca41b1c6
hzj <hezhijian249@qq.com>
2024-12-19 22:26:44 +0800

    feat: 请求路径更改

--- apps/web-naive/src/api/charts/page.ts
+++ apps/web-naive/src/api/charts/page.ts
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 export function getChartData(currentPage: number) {
   return axios({
-    url: `/mock-charts/page/${currentPage}.json`,
+    url: `/mock-charts/page/${Math.ceil(currentPage / 50)}/${currentPage}.json`,
     method: 'get',
   });
 }
 
 export function getChartOptions(cid: string) {
   return axios({
-    url: `/mock-charts/code/${cid}.json`,
+    url: `/mock-charts/code/${[...cid].slice(2, 4).join('/')}/${cid}.json`,
     method: 'get',
   });
 }
