662ba1010dcfd404f4723b76e0bc8cd6943c1e82
hzj <hezhijian249@qq.com>
2024-12-20 22:42:20 +0800

    feat: 补充地图资源加载路径

--- apps/web-naive/src/utils/executeScript.ts
+++ apps/web-naive/src/utils/executeScript.ts
@@ -5,12 +5,15 @@ export function chartHtml() {
 export function drawChart(codeValue: string) {
   return innerScripts(`
   (() => {
-    var option;
-    ${codeValue};
     const div = document.querySelector('#chart');
     if (div) {
+      var option;
       const chart = echarts.init(div);
-      chart.setOption(option);
+      const myChart = chart;
+      ${codeValue};
+      if (option) {
+        chart.setOption(option);
+      }
       window.onresize = () => {
         chart.resize();
       };
@@ -20,9 +23,10 @@ export function drawChart(codeValue: string) {
 
 export function externalScripts() {
   const urls = [
-    'https://cdn.jsdelivr.net/npm/echarts@5.5.1/dist/echarts.min.js',
+    'https://registry.npmmirror.com/echarts/5.5.1/files/dist/echarts.min.js',
     'https://registry.npmmirror.com/jquery/3.7.1/files/dist/jquery.min.js',
     'https://registry.npmmirror.com/echarts-liquidfill/3.1.0/files/dist/echarts-liquidfill.min.js',
+    'http://localhost:5173/mock-charts/echarts/registryMap.js',
   ];
   return urls.map((url) => `<script src="${url}"></script>`).join('');
 }
@@ -31,7 +35,33 @@ export function innerScripts(value: string) {
   return `<script>${value}</script>`;
 }
 
+function changeLoadGeoUrl(codeValue: string) {
+  let index = 0;
+  let result = '';
+  const regexp = /^\/asset\/get\/s\/data-\d+-([0-9A-Za-z]+\.json)/;
+  while (index < codeValue.length) {
+    const remaining = codeValue.slice(Math.max(0, index));
+    if (regexp.test(remaining)) {
+      const match = remaining.match(regexp);
+      if (match) {
+        const fileName: string = match[1] || '';
+        const first: string = fileName.slice(0, 1);
+        result += `/mock-charts/map/${first.toLowerCase()}/${fileName}`;
+        index += match[0].length - 1;
+      }
+    } else {
+      result += remaining.slice(0, 1);
+    }
+    index++;
+  }
+  return result;
+}
+
 export function iframeValue(codeValue: string) {
-  const result = [externalScripts(), chartHtml(), drawChart(codeValue)];
+  const result = [
+    externalScripts(),
+    chartHtml(),
+    drawChart(changeLoadGeoUrl(codeValue)),
+  ];
   return result.join('');
 }
