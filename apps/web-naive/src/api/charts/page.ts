b91b1c523331f832cb834d0538f89fc92be9d8f3
hzj <hezhijian249@qq.com>
2025-01-20 23:55:51 +0800

    feat: 添加请求地址前缀

--- apps/web-naive/src/api/charts/page.ts
+++ apps/web-naive/src/api/charts/page.ts
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 export function getChartData(currentPage: number) {
   return axios({
-    url: `/mock-charts/page/${Math.ceil(currentPage / 50)}/${currentPage}.json`,
+    url: `https://github.39nyx.cn/mock-charts/page/${Math.ceil(currentPage / 50)}/${currentPage}.json`,
     method: 'get',
   });
 }
 
 export function getChartOptions(cid: string) {
   return axios({
-    url: `/mock-charts/code/${[...cid].slice(2, 4).join('/').toLowerCase()}/${cid}.js`,
+    url: `https://github.39nyx.cn/mock-charts/code/${[...cid].slice(2, 4).join('/').toLowerCase()}/${cid}.js`,
     method: 'get',
   });
 }
